019d6b8887478084df78840d1af77cf78d04fcb5
thiagoburg <thiagoburg.okk@gmail.com>
2026-07-24 13:30:01 -0300

    make installer bootstrap clean linux systems

--- install.sh
+++ install.sh
@@ -6,16 +6,39 @@ REPO="https://github.com/thiagoburg/celvo.git"
 DIR="$HOME/.local/share/celvo"
 VERSION="1.1.2"
 
+
 echo
 echo "==> Installing Celvo v$VERSION"
 echo
+
 
 if ! command -v git >/dev/null 2>&1; then
 
     echo "==> Installing dependencies..."
 
-    sudo apt update >/dev/null
-    sudo apt install -y git >/dev/null
+    if command -v apt >/dev/null 2>&1; then
+
+        sudo apt update >/dev/null
+        sudo apt install -y git >/dev/null
+
+    elif command -v dnf >/dev/null 2>&1; then
+
+        sudo dnf install -y git >/dev/null
+
+    elif command -v pacman >/dev/null 2>&1; then
+
+        sudo pacman -Sy --noconfirm git >/dev/null
+
+    elif command -v zypper >/dev/null 2>&1; then
+
+        sudo zypper install -y git >/dev/null
+
+    else
+
+        echo "Unsupported Linux distribution."
+        exit 1
+
+    fi
 
 fi
 
@@ -34,6 +57,7 @@ else
 
     cd "$DIR"
     git pull -q
+
 fi
 
 
